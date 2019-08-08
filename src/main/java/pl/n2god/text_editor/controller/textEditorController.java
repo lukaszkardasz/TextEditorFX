ffb217f6e944f978e04deee3fcff84e8d486362a
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-08-08 13:48:51 +0200

    add actions

--- src/main/java/pl/n2god/text_editor/controller/textEditorController.java
+++ src/main/java/pl/n2god/text_editor/controller/textEditorController.java
@@ -1,19 +1,57 @@
 package pl.n2god.text_editor.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 
 public class textEditorController {
 
     @FXML
-    private TextArea mainTextArea;
+    private MenuItem openFile;
 
     @FXML
-    private Button lowerCaseButton;
+    private MenuItem closeFile;
 
+    @FXML
+    private Label fileName;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Button changeToLowerCaseButton;
+
+    @FXML
+    private Button clearTextButton;
 
     public void initialize() {
-        System.out.println("text editor controller initialized");
+        //można to zrobić setOnAction
+        changeToLowerCaseButton.setOnAction(event -> {
+            System.out.print("Wciśnięto przycisk:");
+            System.out.println(event.getEventType());
+            String originalText = textArea.getText();
+            String lowerText = originalText.toLowerCase();
+            textArea.setText(lowerText);
+        });
+        //lub można to zrobić addEventFilter lub addEventHandler - bez różnicy - tylko tutaj można obsługiwać także klawiaturę i mysz
+        clearTextButton.addEventFilter(ActionEvent.ACTION, event -> {
+            System.out.print("Wciśnięto przycisk: ");
+            System.out.println(event.getEventType());
+            textArea.setText("");
+        });
+        //o tu np. obsługujemy klawiaturę
+        textArea.addEventFilter(KeyEvent.KEY_TYPED, keyEvent ->{
+            System.out.println("Wciśnięto: " + keyEvent.getCharacter());
+        });
+        //a tu zamykamy proram handlerem bo tak sobie wymyśliłem...
+        closeFile.addEventHandler(ActionEvent.ACTION, actionEvent -> {
+            System.out.println(actionEvent.getEventType());
+            System.out.println("Zakończono program!");
+            System.exit(0);
+        });
     }
 }
