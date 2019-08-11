7ef3dadc970dafc07d1726d09084a9196640efde
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-08-11 17:55:35 +0200

    button actions added

--- src/main/java/pl/n2god/text_editor/controller/MainController.java
+++ src/main/java/pl/n2god/text_editor/controller/MainController.java
@@ -1,11 +1,30 @@
 package pl.n2god.text_editor.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 /**
  * @author n2god on 09/08/2019
  * @project TextEditor
  */
 
 public class MainController {
-//TODO dodać strukturę main controlera
+    @FXML
+    private ContentPaneController contentPaneController;
+
+    @FXML
+    private ControlPaneController controlPaneController;
+
+    public void initialize(){
+        Button lowercaseButton = controlPaneController.getChangeToLowerCaseButton();
+        Button upperCaseButton = controlPaneController.getToUpperCaseButton();
+        Button clearButton = controlPaneController.getClearTextButton();
+        TextArea textArea = contentPaneController.getMainTextArea();
+
+        lowercaseButton.setOnAction(x -> textArea.setText(textArea.getText().toLowerCase()));
+        upperCaseButton.setOnAction(x -> textArea.setText(textArea.getText().toUpperCase()));
+        clearButton.setOnAction(x -> textArea.clear());
+    }
 }
 
